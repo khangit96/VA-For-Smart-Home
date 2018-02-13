97ca19d56386c79c87ca99dd55cde47b987f2963
tdmuteam <tdmuteam2016@gmail.com>
2018-02-13 11:26:17 +0000

    Add receiveStream.py

---
+++ receiveStream.py
@@ -0,0 +1,28 @@
+import cv2
+import urllib.request
+import numpy as np
+
+#Camera raspberry stream over MJPG Streamer server
+#Read stream
+stream= urllib.request.urlopen('http://192.168.43.164:8080/?action=stream')
+bytes = bytes()
+
+while True:
+    #get byte from stream
+    bytes += stream.read(1024)
+    a = bytes.find(b'\xff\xd8')
+    b = bytes.find(b'\xff\xd9')
+    
+    if a != -1 and b != -1:
+        jpg = bytes[a:b+2]
+        bytes = bytes[b+2:]
+        
+        #Decode byte to img
+        i = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
+        
+        #showimg
+        cv2.imshow('i', i)
+        
+        #Exit loop
+        if cv2.waitKey(1) == 27:
+            exit(0)
